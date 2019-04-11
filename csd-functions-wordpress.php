6f03f2e13db0014668fd0024aa7f4d2d8b241332
Josh Armentano <josh@abidewebdesign.com>
2019-04-11 07:59:22 -0700

    Moved custom post type registration away from Toolset Types.

--- csd-functions-wordpress.php
+++ csd-functions-wordpress.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: CSD Functions - Wordpress Admin
-Version: 1.4
-Description: Wordpress Admin Customizations for CSD School and District Theme
+Version: 1.5
+Description: Wordpress Admin Customizations and Custom Post Types for CSD School and District Theme
 Author: Josh Armentano
 Author URI: https://abidewebdesign.com
 Plugin URI: https://abidewebdesign.com
@@ -33,7 +33,8 @@ function csd_admin_bar_render() {
     $wp_admin_bar->remove_menu('widgets');
     $wp_admin_bar->remove_node('updates');
     $wp_admin_bar->remove_menu('searchwp');
-
+    $wp_admin_bar->remove_menu('delete-cache');
+$wp_admin_bar->remove_menu('litespeed-menu');
 }
 add_action( 'wp_before_admin_bar_render', 'csd_admin_bar_render' );
 
@@ -127,3 +128,260 @@ function admin_bar_style_override() {
 }
 add_action('wp_head', 'admin_bar_style_override');
 add_action('admin_head', 'admin_bar_style_override');
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: News.
+	 */
+
+	$labels = array(
+		"name" => __( "News", "custom-post-type-ui" ),
+		"singular_name" => __( "News", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "News", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "news", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-megaphone",
+		"supports" => array( "title" ),
+	);
+
+	register_post_type( "news", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
+function cptui_register_my_cpts_news() {
+
+	/**
+	 * Post Type: News.
+	 */
+
+	$labels = array(
+		"name" => __( "News", "custom-post-type-ui" ),
+		"singular_name" => __( "News", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "News", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "news", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-megaphone",
+		"supports" => array( "title" ),
+	);
+
+	register_post_type( "news", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_news' );
+
+function cptui_register_my_taxes() {
+
+	/**
+	 * Taxonomy: News Categories.
+	 */
+
+	$labels = array(
+		"name" => __( "News Categories", "custom-post-type-ui" ),
+		"singular_name" => __( "News Category", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "News Categories", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'news-category', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "news-category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "news-category", array( "news" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
+
+function cptui_register_my_taxes_news_category() {
+
+	/**
+	 * Taxonomy: News Categories.
+	 */
+
+	$labels = array(
+		"name" => __( "News Categories", "custom-post-type-ui" ),
+		"singular_name" => __( "News Category", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "News Categories", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'news-category', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "news-category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "news-category", array( "news" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_news_category' );
+
+function cptui_register_my_cpts_emergency_alerts() {
+
+	/**
+	 * Post Type: Alerts.
+	 */
+
+	$labels = array(
+		"name" => __( "Alerts", "custom-post-type-ui" ),
+		"singular_name" => __( "Alert", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "Alerts", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => true,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "emergency-alerts", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-shield",
+		"supports" => array( "title" ),
+	);
+
+	register_post_type( "emergency-alerts", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_emergency_alerts' );
+
+function cptui_register_my_cpts_directory() {
+
+	/**
+	 * Post Type: Directory.
+	 */
+
+	$labels = array(
+		"name" => __( "Directory", "custom-post-type-ui" ),
+		"singular_name" => __( "Directory", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "Directory", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => true,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "directory", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-businessman",
+		"supports" => array( "title" ),
+	);
+
+	register_post_type( "directory", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_directory' );
+
+function cptui_register_my_taxes_directory_category() {
+
+	/**
+	 * Taxonomy: Categories.
+	 */
+
+	$labels = array(
+		"name" => __( "Categories", "custom-post-type-ui" ),
+		"singular_name" => __( "Category", "custom-post-type-ui" ),
+	);
+
+	$args = array(
+		"label" => __( "Categories", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'directory-category', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "directory-category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "directory-category", array( "directory" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_directory_category' );
+
